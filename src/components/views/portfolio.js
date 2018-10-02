fab4ba4f1dac84583050ba604b24bdd427468f30
Usheninte <jrandangana@gmail.com>
2018-10-02 09:00:10 +0100

    building Portfolio

--- src/components/views/portfolio.js
+++ src/components/views/portfolio.js
@@ -3,14 +3,8 @@ import { Image } from 'react-bootstrap';
 import { Grid } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 
-const smallBox = {
-  width: "400px",
-  height: "400px"
-}
-
-const bigBox = {
-  width: "600px",
-  height: "600px"
+const imgSpace = {
+  marginBottom: "3px"
 }
 
 export default class Portfolio extends Component {
@@ -21,47 +15,47 @@ export default class Portfolio extends Component {
         <Grid>
           <Col sm={3}>
             <Col xs={12}>
-              <Image style={smallBox} src={''} rounded responsive />
+              <Image style={imgSpace} src={'https://res.cloudinary.com/poetrique/image/upload/v1538098780/clients/RoyalsCollectives/6LACK-01.png'} rounded responsive />
             </Col>
             <Col xs={12}>
-              <Image style={smallBox} src={''} rounded responsive />
+              <Image style={imgSpace} src={'https://res.cloudinary.com/poetrique/image/upload/v1538098811/clients/RoyalsCollectives/kanyeee-01_1.png'} rounded responsive />
             </Col>
             <Col xs={12}>
-              <Image style={smallBox} src={''} rounded responsive />
+              <Image style={imgSpace} src={''} rounded responsive />
             </Col>
             <Col xs={12}>
-              <Image style={smallBox} src={''} rounded responsive />
+              <Image style={imgSpace} src={''} rounded responsive />
             </Col>
             <Col xs={12}>
-              <Image style={smallBox} src={''} rounded responsive />
+              <Image style={imgSpace} src={''} rounded responsive />
             </Col>
           </Col>
           <Col sm={6}>
             <Col sm={12}>
-              <Image style={bigBox} src={'https://res.cloudinary.com/poetrique/image/upload/v1538098780/clients/RoyalsCollectives/6LACK-01.png'} rounded responsive />
+              <Image style={imgSpace} src={'https://res.cloudinary.com/poetrique/image/upload/v1538098753/clients/RoyalsCollectives/xxx-TENTACION.jpg'} rounded responsive />
             </Col>
             <Col sm={12}>
-              <Image style={bigBox} src={'https://res.cloudinary.com/poetrique/image/upload/v1538098753/clients/RoyalsCollectives/xxx-TENTACION.jpg'} rounded responsive />
-            </Col>
+              <Image style={imgSpace} src={'https://res.cloudinary.com/poetrique/image/upload/v1538098818/clients/RoyalsCollectives/final_eazi.png'} rounded responsive />
             <Col sm={12}>
-              <Image style={bigBox} src={'https://res.cloudinary.com/poetrique/image/upload/v1538098752/clients/RoyalsCollectives/belly-01.png'} rounded responsive />
+              <Image style={imgSpace} src={''} rounded responsive />
+            </Col>
             </Col>
           </Col>
           <Col sm={3}>
             <Col xs={12}>
-              <Image style={smallBox} src={''} rounded responsive />
+              <Image style={imgSpace} src={'https://res.cloudinary.com/poetrique/image/upload/v1538098752/clients/RoyalsCollectives/belly-01.png'} rounded responsive />
             </Col>
             <Col xs={12}>
-              <Image style={smallBox} src={''} rounded responsive />
+              <Image style={imgSpace} src={'https://res.cloudinary.com/poetrique/image/upload/v1538098784/clients/RoyalsCollectives/wiz-01.png'} rounded responsive />
             </Col>
             <Col xs={12}>
-              <Image style={smallBox} src={''} rounded responsive />
+              <Image style={imgSpace} src={''} rounded responsive />
             </Col>
             <Col xs={12}>
-              <Image style={smallBox} src={''} rounded responsive />
+              <Image style={imgSpace} src={''} rounded responsive />
             </Col>
             <Col xs={12}>
-              <Image style={smallBox} src={''} rounded responsive />
+              <Image style={imgSpace} src={''} rounded responsive />
             </Col>
           </Col>
         </Grid>
