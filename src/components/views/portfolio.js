430f60e4c528faccfbf305b9651551cc06139ddc
Usheninte <jrandangana@gmail.com>
2018-10-02 08:39:19 +0100

    portfolio restart

--- src/components/views/portfolio.js
+++ src/components/views/portfolio.js
@@ -11,33 +11,18 @@ export default class Portfolio extends Component {
         <Grid>
           <Col sm={3}>
             <Col xs={12}>
-              <Image src={'https://res.cloudinary.com/poetrique/image/upload/v1538098818/clients/RoyalsCollectives/final_eazi.png'} rounded responsive />
-            </Col>
-            <Col xs={12}>
               <Image src={''} rounded responsive />
             </Col>
           </Col>
           <Col sm={6}>
-            <Image src={'https://res.cloudinary.com/poetrique/image/upload/v1538098816/clients/RoyalsCollectives/stop_trying_to_be_god-01.png'} rounded responsive />
+            <Col sm={12}>
+              <Image src={''} rounded responsive />
+            </Col>
           </Col>
           <Col sm={3}>
             <Col xs={12}>
-              <Image src={'https://res.cloudinary.com/poetrique/image/upload/v1538098809/clients/RoyalsCollectives/da_slump_god.png'} rounded responsive />
-            </Col>
-            <Col xs={12}>
               <Image src={''} rounded responsive />
             </Col>
-          </Col>
-        </Grid>
-        <Grid>
-          <Col xsHidden sm={3}>
-            <Image src={''} rounded responsive />
-          </Col>
-          <Col sm={6}>
-            <Image src={''} rounded responsive />
-          </Col>
-          <Col xsHidden sm={3}>
-            <Image src={''} rounded responsive />
           </Col>
         </Grid>
       </div>
