dbcc5053a82e3f076a8413a8286ba8b5638c8554
Usheninte <jrandangana@gmail.com>
2018-10-02 08:48:30 +0100

    added images to Portfolio

--- src/components/views/portfolio.js
+++ src/components/views/portfolio.js
@@ -3,6 +3,16 @@ import { Image } from 'react-bootstrap';
 import { Grid } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 
+const smallBox = {
+  width: "400px",
+  height: "400px"
+}
+
+const bigBox = {
+  width: "600px",
+  height: "600px"
+}
+
 export default class Portfolio extends Component {
   render() {
     return(
@@ -11,17 +21,47 @@ export default class Portfolio extends Component {
         <Grid>
           <Col sm={3}>
             <Col xs={12}>
-              <Image src={''} rounded responsive />
+              <Image style={smallBox} src={''} rounded responsive />
+            </Col>
+            <Col xs={12}>
+              <Image style={smallBox} src={''} rounded responsive />
+            </Col>
+            <Col xs={12}>
+              <Image style={smallBox} src={''} rounded responsive />
+            </Col>
+            <Col xs={12}>
+              <Image style={smallBox} src={''} rounded responsive />
+            </Col>
+            <Col xs={12}>
+              <Image style={smallBox} src={''} rounded responsive />
             </Col>
           </Col>
           <Col sm={6}>
             <Col sm={12}>
-              <Image src={''} rounded responsive />
+              <Image style={bigBox} src={'https://res.cloudinary.com/poetrique/image/upload/v1538098780/clients/RoyalsCollectives/6LACK-01.png'} rounded responsive />
+            </Col>
+            <Col sm={12}>
+              <Image style={bigBox} src={'https://res.cloudinary.com/poetrique/image/upload/v1538098753/clients/RoyalsCollectives/xxx-TENTACION.jpg'} rounded responsive />
+            </Col>
+            <Col sm={12}>
+              <Image style={bigBox} src={'https://res.cloudinary.com/poetrique/image/upload/v1538098752/clients/RoyalsCollectives/belly-01.png'} rounded responsive />
             </Col>
           </Col>
           <Col sm={3}>
             <Col xs={12}>
-              <Image src={''} rounded responsive />
+              <Image style={smallBox} src={''} rounded responsive />
+            </Col>
+            <Col xs={12}>
+              <Image style={smallBox} src={''} rounded responsive />
+            </Col>
+            <Col xs={12}>
+              <Image style={smallBox} src={''} rounded responsive />
+            </Col>
+            <Col xs={12}>
+              <Image style={smallBox} src={''} rounded responsive />
+            </Col>
+            <Col xs={12}>
+              <Image style={smallBox} src={''} rounded responsive />
             </Col>
           </Col>
         </Grid>
